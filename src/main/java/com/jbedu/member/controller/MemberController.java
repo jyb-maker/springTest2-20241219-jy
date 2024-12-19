8103696db64073846eb1c3e821f42a71585e0f98
jyb-maker <bjy7328@naver.com>
2024-12-19 10:15:13 +0900

    CONTROLLER 예제

---
+++ src/main/java/com/jbedu/member/controller/MemberController.java
@@ -0,0 +1,39 @@
+package com.jbedu.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller  //어노테이션 Controller->컨트롤러 클래스로 지정
+public class MemberController {
+		
+	@RequestMapping(value = "/")   // root 요청
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping(value = "/baseball")   // value값 -> 요청(request) 값
+	public String baseball() {
+		return "baseball";
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/webapp/WEB-INF/views/baseball.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>프로야구</title>
+</head>
+<body>
+	<h2>프로야구 소식</h2>
+	<hr>
+	오늘의 프로구야구 소식을 알려드립니다.
+	
+</body>
+</html>
