cabf8d52cc45dbf9a5ae40aee1f16e264cdec03a
jyb-maker <bjy7328@naver.com>
2024-12-19 14:24:33 +0900

    어노테이션을 이용한 파라미터 값 받기 추가

--- src/main/java/com/jbedu/member/controller/DataController.java
+++ src/main/java/com/jbedu/member/controller/DataController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -45,6 +46,12 @@ public class DataController {
 		return "login2";
 	}
 	
+	@RequestMapping(value = "/login3") //login3.jsp를 실행시켜주는 역할만!
+	public String login3() {
+		
+		return "login3";
+	}
+	
 	@RequestMapping(value = "/confirmID") //클라이언트의 로그인 요청을 여기서 catch!(파라미터 값도 함께)
 	public String confirmID(HttpServletRequest request, Model model) {
 		
@@ -68,6 +75,22 @@ public class DataController {
 		
 		String mid = request.getParameter("mid");
 		String mpw = request.getParameter("mpw");
+		
+		if(mid.equals("tiger") && mpw.equals("12345")) { //로그인 성공
+			model.addAttribute("idcheck", "memberOk");
+			model.addAttribute("loginid", mid);
+			model.addAttribute("loginpw", mpw);
+		} else { //로그인 실패
+			model.addAttribute("idcheck", "memberNo");
+		}
+		return "checkID";
+	}
+	
+	@RequestMapping(value = "/checkID2") //클라이언트의 로그인 요청을 여기서 catch!(파라미터 값도 함께)
+	public String checkID2(@RequestParam("mid") String mid, @RequestParam("mpw") String mpw, Model model) {
+		
+//		String mid = request.getParameter("mid");
+//		String mpw = request.getParameter("mpw");
 		
 		if(mid.equals("tiger") && mpw.equals("12345")) { //로그인 성공
 			model.addAttribute("idcheck", "memberOk");
